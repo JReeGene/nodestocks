8c0dfccf85b2bcaa4c06902019f687808b7a7a0f
Jeremiah Gitaga <jgitaga@gmail.com>
2023-11-17 14:22:29 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        fruits: "Oranges"
+    }); 
+});
+
+app.listen(3000);
